1fe1ba9b8a2e5e51da81b9387473436006285d2c
OlgaKostiuk <olga.d.kostiuk@gmail.com>
2017-11-11 11:50:50 +0200

    realJob tests

--- TestCalc/UnitTest1.cs
+++ TestCalc/UnitTest1.cs
@@ -7,31 +7,6 @@ using TestStack.White.UIItems;
 using TestStack.White.UIItems.WindowItems;
 using TestStack.White.UIItems.Finders;
 
-//namespace TestCalc
-//{
-//    [TestClass]
-//    public class UnitTest1
-//    {
-//        [DataTestMethod]
-//        [DataRow("text_a", true)]
-//        [DataRow("text_b", true)]
-//        [DataRow("text_op", true)]
-//        [DataRow("text_res", true)]
-//        public void Calc_Enable(string id, bool exp)
-//        {
-//            Application application = Application.Launch(new ProcessStartInfo(@"CalcWinForm.exe")
-//            {
-//                WorkingDirectory = @"..\..\..\CalcWinForm\bin\Debug\",
-//            });
-//            Window window = application.GetWindows()[0];
-
-//            bool res = window.Get<TextBox>(SearchCriteria.ByAutomationId(id)).Enabled;
-//            Assert.AreEqual(exp, res);
-//            application.Kill();
-//        }
-//    }
-//}
-
 namespace TestCalc
 {
     [TestClass]
@@ -141,8 +116,9 @@ namespace TestCalc
             window.Get<TextBox>(SearchCriteria.ByAutomationId("text_op")).SetValue(op);
             window.Get<Button>(SearchCriteria.ByAutomationId("btn_res")).Click();
             string res = window.Get<TextBox>(SearchCriteria.ByAutomationId("text_res")).Text;
-            Assert.AreEqual(exp, res);
+
             application.Kill();
+            Assert.AreEqual(exp, res);
         }
     }
 
